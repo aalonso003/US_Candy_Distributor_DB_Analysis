c6f02d6b5bd1dda1bfd68abe6137345d7a385c0f
aalonso003 <alexander.alonso003@mymdc.net>
2026-04-20 21:46:46 -0400

    Update US_Candy_Distributor-SQL.sql

--- SQL/US_Candy_Distributor-SQL.sql
+++ SQL/US_Candy_Distributor-SQL.sql
@@ -1,5 +1,8 @@
-
--- 1. Total Revenue per Year
+-- ============================================================
+-- QUERY 1. Total Revenue per Year
+-- Goal: See YoY revenue for the Company
+-- Skills:  GROUP BY, ORDER BY, aggregate functions
+-- ============================================================
 SELECT 
     CAST(YEAR(OrderDate) AS VARCHAR(4)) AS Year,
     SUM(Revenue) AS TotalRevenue
@@ -8,46 +11,34 @@ GROUP BY YEAR(OrderDate)
 ORDER BY YEAR(OrderDate);
 
 
-
--- 2. Top 5 selling products by revenue
-SELECT TOP (5) P.ProductName, SUM(S.UnitsSold) AS TotalUnits, SUM(S.Revenue) AS TotalRevenue
-FROM Sales S
-JOIN Products P ON S.ProductID = P.ProductID
-GROUP BY P.ProductName
+-- ============================================================
+-- QUERY 2: Top 5 Best-Selling Products by Revenue
+-- Goal: Identify which individual products generate the most
+--       revenue for the distributor.
+-- Skills: JOIN, GROUP BY, ORDER BY, TOP
+-- ============================================================
+SELECT TOP 5
+    p.ProductName,
+    p.Division,
+    SUM(s.UnitsSold)  AS TotalUnitsSold,
+    SUM(s.Revenue)    AS TotalRevenue
+FROM Sales s
+JOIN Products p ON s.ProductID = p.ProductID
+GROUP BY p.ProductName, p.Division
 ORDER BY TotalRevenue DESC;
 
 
 
 -- 3. Total Sales per factory
+-- ============================================================
+-- QUERY 3. Total Sales per factory
+-- Goal: Identify the Total Sales per Factory
+-- Skills: JOIN, GROUP BY, ORDER BY
+-- ============================================================
 SELECT F.FactoryName, SUM(S.Revenue) AS FactoryRevenue
 FROM Sales S
 JOIN Products P ON S.ProductID = P.ProductID
 JOIN Factories F ON P.FactoryID = F.FactoryID
 GROUP BY F.FactoryName
 ORDER BY FactoryRevenue DESC;
-
-/* 4- Creating A Factory Report by joining sales, products, and factories into a 
-single analysis with multiple JOINS making it more accessible  */
-GO
-CREATE VIEW dbo.vFactoryReportV AS
-SELECT S.OrderDate, SUM(S.Revenue) AS TotalRevenue, P.ProductName, F.FactoryName
-FROM Sales S
-INNER JOIN Products P  
-    ON S.ProductID = P.ProductID
-INNER JOIN Factories F
-    ON p.FactoryID = F.FactoryID
-GROUP BY S.OrderDate, P.ProductName, F.FactoryName
-GO
-;
-
-SELECT * FROM vFactoryReportV;
-
- -- 5. Identify what are the best perfomance-based factories
-SELECT F.FactoryName, COUNT(*) AS TotalSales
-FROM Factories F
-INNER JOIN Products P
-    ON F.FactoryID = P.FactoryID
-GROUP BY F.FactoryName
-HAVING COUNT(*) > 3;
-
 
